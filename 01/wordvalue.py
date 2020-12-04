8ca922d409a3b9dff0f6ab6426740f2665497386
Nash <nashakpd@gmail.com>
2020-12-04 11:40:10 +0100

    added wordvalue function

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,22 +1,34 @@
 from data import DICTIONARY
-from data import LETTER_SCORES as SCORES_DICT
+from data import LETTER_SCORES
 
 def load_words():
     """Load DICTIONARY file contents into a list and return list"""
     with open(DICTIONARY, 'r') as file:
         content = file.readlines()
         wordlist = [line.strip() for line in content]
-        return wordlist
 
-def calc_word_value():
+    return wordlist
+
+
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    value = 0
+    letters = list(word.upper())
+    for i in letters:
+        value += LETTER_SCORES[i]
+
+    return value
+    
 
 def max_word_value():
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
     pass
 
+
 if __name__ == "__main__":
+    calc_word_value("apple")
     pass # run unittests to validate
+
+
